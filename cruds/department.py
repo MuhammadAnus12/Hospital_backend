0787d95dd83a2735bd9d07ae1cef641a4c0ecfcb
MuhammadAnus <muhammadanus2004@gmail.com>
2024-07-06 19:25:07 +0500

    refactor: Add head of department relationship to department model

--- cruds/department.py
+++ cruds/department.py
@@ -2,7 +2,8 @@ from sqlalchemy.orm import Session
 from models import (department as model_department,
                     doctor as model_doctor,
                     nurse as model_nurse,
-                    room as model_room)
+                    room as model_room,
+                    head_of_department as model_head_of_department)
 from schemas import department as schema_department
 from typing import List
 
@@ -14,8 +15,10 @@ def create_department(db: Session, department: schema_department.DepartmentCreat
     db.refresh(db_department)
     return db_department
 
+
 def get_departments(db: Session, skip: int = 0, limit: int = 100):
     return db.query(model_department.Department).offset(skip).limit(limit).all()
+
 
 def get_department(db: Session, department_id: int):
     db_department = db.query(model_department.Department).filter(
@@ -23,6 +26,7 @@ def get_department(db: Session, department_id: int):
     if db_department is None:
         return None
     return db_department
+
 
 def update_department(db: Session, department: schema_department.DepartmentUpdate):
     db_department = db.query(model_department.Department).filter(
@@ -35,6 +39,7 @@ def update_department(db: Session, department: schema_department.DepartmentUpdat
     db.refresh(db_department)
     return db_department
 
+
 def delete_department(db: Session, department_id: int):
     db_department = db.query(model_department.Department).filter(
         model_department.Department.id == department_id).first()
@@ -44,6 +49,7 @@ def delete_department(db: Session, department_id: int):
     db.commit()
     return db_department
 
+
 def search_department(db: Session, department: schema_department.SearchDepartment):
     db_department = db.query(model_department.Department).filter(
         model_department.Department.name == department.name).first()
@@ -51,11 +57,14 @@ def search_department(db: Session, department: schema_department.SearchDepartmen
         return None
     return db_department
 
-def check_department_child(db:Session,department_id:int)->bool:
-    if db.query(model_doctor.Doctor).filter(model_doctor.Doctor.department_id==department_id).first():
+
+def check_department_child(db: Session, department_id: int) -> bool:
+    if db.query(model_doctor.Doctor).filter(model_doctor.Doctor.department_id == department_id).first():
         return True
-    if db.query(model_nurse.Nurse).filter(model_nurse.Nurse.department_id==department_id).first():
+    if db.query(model_nurse.Nurse).filter(model_nurse.Nurse.department_id == department_id).first():
         return True
-    if db.query(model_room.Room).filter(model_room.Room.department_id==department_id).first():
+    if db.query(model_room.Room).filter(model_room.Room.department_id == department_id).first():
+        return True
+    if db.query(model_head_of_department.HeadOfDepartment).filter(model_head_of_department.HeadOfDepartment.department_id == department_id).first():
         return True
     return False
