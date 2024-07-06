ac71e1c3d8ce5dbd163ef48e8b03eab770f30a54
MuhammadAnus <muhammadanus2004@gmail.com>
2024-07-06 12:06:55 +0500

    chore: Add CRUD operations for doctor-related models and update doctor router

--- cruds/doctor.py
+++ cruds/doctor.py
@@ -1,26 +1,51 @@
 from sqlalchemy.orm import Session
-from models import doctor as model_doctor
+from models import (doctor as model_doctor,
+                    room as model_room, department as model_department,
+                    appointment as model_appointment, treatment as model_treatment
+                    )
 from schemas import doctor as schema_doctor
 from typing import List
 
+
 def create_doctor(db: Session, doctor: schema_doctor.DoctorCreate):
-    db_doctor=model_doctor.Doctor(**doctor.model_dump())
+    db_doctor = model_doctor.Doctor(**doctor.model_dump())
     db.add(db_doctor)
     db.commit()
     db.refresh(db_doctor)
     return db_doctor
 
+
+def check_room(db: Session, room_id: int):
+    db_room = db.query(model_room.Room).filter(
+        model_room.Room.id == room_id).first()
+    if db_room is None:
+        return None
+    return db_room
+
+
+def check_department(db: Session, department_id: int):
+    db_department = db.query(model_department.Department).filter(
+        model_department.Department.id == department_id).first()
+    if db_department is None:
+        return None
+    return db_department
+
+
 def get_doctor(db: Session, doctor_id: int):
-    db_doctor = db.query(model_doctor.Doctor).filter(model_doctor.Doctor.id == doctor_id).first()
+    db_doctor = db.query(model_doctor.Doctor).filter(
+        model_doctor.Doctor.id == doctor_id).first()
     if db_doctor is None:
         return None
     return db_doctor
 
+
 def get_doctors(db: Session, skip: int = 0, limit: int = 100):
     return db.query(model_doctor.Doctor).offset(skip).limit(limit).all()
 
+
 def update_doctor(db: Session, doctor: schema_doctor.DoctorUpdate):
-    db_doctor = db.query(model_doctor.Doctor).filter(model_doctor.Doctor.id == doctor.id).first()
+    db_doctor = db.query(model_doctor.Doctor).filter(
+        model_doctor.Doctor.id == doctor.id).first()
     if db_doctor is None:
         return None
     for key, value in doctor.model_dump().items():
@@ -29,16 +54,28 @@ def update_doctor(db: Session, doctor: schema_doctor.DoctorUpdate):
     db.refresh(db_doctor)
     return db_doctor
 
+
 def delete_doctor(db: Session, doctor_id: int):
-    db_doctor = db.query(model_doctor.Doctor).filter(model_doctor.Doctor.id == doctor_id).first()
+    db_doctor = db.query(model_doctor.Doctor).filter(
+        model_doctor.Doctor.id == doctor_id).first()
     if db_doctor is None:
         return None
     db.delete(db_doctor)
     db.commit()
     return db_doctor
 
+
 def search_doctor(db: Session, doctor: schema_doctor.SearchDoctor):
-    db_doctor = db.query(model_doctor.Doctor).filter(model_doctor.Doctor.first_name == doctor.first_name).filter(model_doctor.Doctor.last_name == doctor.last_name).filter(model_doctor.Doctor.specialty == doctor.specialty).first()
+    db_doctor = db.query(model_doctor.Doctor).filter(model_doctor.Doctor.first_name == doctor.first_name).filter(
+        model_doctor.Doctor.last_name == doctor.last_name).filter(model_doctor.Doctor.specialty == doctor.specialty).first()
     if db_doctor is None:
         return None
     return db_doctor
+
+
+def check_doctor_in_child(db: Session, doctor_id: int) -> bool:
+    if db.query(model_appointment.Appointment).filter(model_appointment.Appointment.doctor_id == doctor_id).first():
+        return True
+    if db.query(model_treatment.Treatment).filter(model_treatment.Treatment.doctor_id == doctor_id).first():
+        return True
+    return False
--- routers/doctor.py
+++ routers/doctor.py
@@ -9,9 +9,17 @@ from typing import List
 router = APIRouter()
 tags = ['Doctor']
 
-@router.post('/doctor', response_model=schema_doctor.Doctor ,tags=tags)
+
+@router.post('/doctor', response_model=schema_doctor.Doctor, tags=tags)
 def create_doctor(doctor: schema_doctor.DoctorCreate, db: Session = Depends(get_db)):
+    db_room = crud_doctor.check_room(db, doctor.room_id)
+    if db_room is None:
+        raise HTTPException(status_code=404, detail="Room not found")
+    db_department = crud_doctor.check_department(db, doctor.department_id)
+    if db_department is None:
+        raise HTTPException(status_code=404, detail="Department not found")
     return crud_doctor.create_doctor(db, doctor)
+
 
 @router.get('/doctor/{doctor_id}', response_model=schema_doctor.Doctor, tags=tags)
 def get_doctor(doctor_id: int, db: Session = Depends(get_db)):
@@ -19,6 +27,7 @@ def get_doctor(doctor_id: int, db: Session = Depends(get_db)):
     if db_doctor is None:
         raise HTTPException(status_code=404, detail='Doctor not found')
     return db_doctor
+
 
 @router.get('/doctors', response_model=schema_doctor.Doctors, tags=tags)
 def get_doctors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -29,20 +38,32 @@ def get_doctors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
         'total_count': total_count
     }
 
+
 @router.put('/doctors', response_model=schema_doctor.Doctor, tags=tags)
 def update_doctor(doctor: schema_doctor.DoctorUpdate, db: Session = Depends(get_db)):
     db_doctor = crud_doctor.get_doctor(db, doctor.id)
     if db_doctor is None:
         raise HTTPException(status_code=404, detail='Doctor not found')
+    db_room = crud_doctor.check_room(db, doctor.room_id)
+    if db_room is None:
+        raise HTTPException(status_code=404, detail="Room not found")
+    db_department = crud_doctor.check_department(db, doctor.department_id)
+    if db_department is None:
+        raise HTTPException(status_code=404, detail="Department not found")
     db_doctor = crud_doctor.update_doctor(db, doctor)
     return db_doctor
 
-@router.delete('/doctor/{doctor_id}', response_model=schema_doctor.Doctor, tags=tags)
+
+@router.delete('/doctor/{doctor_id}', tags=tags)
 def delete_doctor(doctor_id: int, db: Session = Depends(get_db)):
-    db_doctor = crud_doctor.delete_doctor(db, doctor_id)
+    db_doctor = crud_doctor.get_doctor(db, doctor_id)
     if db_doctor is None:
         raise HTTPException(status_code=404, detail='Doctor not found')
-    return db_doctor
+    db_doctor_in_child = crud_doctor.check_doctor_in_child(db, doctor_id)
+    if db_doctor_in_child is True:
+        raise HTTPException(status_code=404, detail="Doctor Exist in Child")
+    return crud_doctor.delete_doctor(db, doctor_id)
+
 
 @router.post('/doctor/search', response_model=schema_doctor.Doctor, tags=tags)
 def search_doctor(doctor: schema_doctor.SearchDoctor, db: Session = Depends(get_db)):
